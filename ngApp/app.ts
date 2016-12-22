0bc26b1a38ac0b04d72b327cce52b714c4de311b
Jordan Christley <lilj390150@live.com>
2016-12-21 18:16:37 -0600

    Jordan finished the state architecture

---
+++ ngApp/app.ts
@@ -0,0 +1,109 @@
+namespace codingskills {
+
+    angular.module('codingskills', ['ui.router', 'ngResource', 'ngMaterial']).config((
+        $stateProvider: ng.ui.IStateProvider,
+        $urlRouterProvider: ng.ui.IUrlRouterProvider,
+        $locationProvider: ng.ILocationProvider
+    ) => {
+        // Define routes
+        $stateProvider
+            .state('nav', {
+                templateUrl: '/ngApp/views/nav.html',
+                controller: codingskills.Controllers.NavController,
+                abstract: true,
+                controllerAs: 'vm'
+            })
+            .state('home', {
+                url: '/',
+                templateUrl: '/ngApp/views/home.html',
+                controller: codingskills.Controllers.HomeController,
+                controllerAs: 'controller',
+                parent: 'nav'
+            })
+
+
+
+
+
+            .state('gym', {
+                url: '/gym', 
+                templateUrl: '/ngApp/views/gym.html',
+                controller: codingskills.Controllers.GymController,
+                controllerAs: 'controller',
+                parent: 'nav',
+            })
+            .state('courtside', {
+                url: '/courtside',
+                templateUrl: '/ngApp/views/courtside.html',
+                controller: codingskills.Controllers.CourtsideController,
+                controllerAs: 'controller',
+                parent: 'gym'
+            })
+            .state('lockerroom', {
+                url: '/lockerroom',
+                templateUrl: '/ngApp/views/lockerroom.html',
+                controller: codingskills.Controllers.LockerroomController,
+                controllerAs: 'controller',
+                parent: 'gym'
+            })
+            .state('scoreboard', {
+                url: '/scoreboard',
+                templateUrl: '/ngApp/views/scoreboard.html',
+                controller: codingskills.Controllers.ScoreboardController,
+                controllerAs: 'controller',
+                parent: 'gym'
+            })
+
+
+
+
+
+
+            .state('account', {
+                url: '/account',
+                templateUrl: '/ngApp/views/account.html',
+                controller: codingskills.Controllers.AccountController,
+                parent: 'nav'
+            })
+            .state('myaccount', {
+                url: '/:username',
+                templateUrl: '/ngApp/views/myaccount.html',
+                controller: codingskills.Controllers.MyAccountController,
+                controllerAs: "controller",
+                parent: 'account'
+            })
+            .state('loginregister', {
+                url: '/registration',
+                templateUrl: '/ngApp/views/loginregister.html',
+                controller: codingskills.Controllers.LoginRegisterController,
+                controllerAs: "controller",
+                parent: 'account'
+            })
+
+
+
+
+
+            .state('about', {
+                url: '/about',
+                templateUrl: '/ngApp/views/about.html',
+                controller: codingskills.Controllers.AboutController,
+                controllerAs: 'controller',
+                parent: 'nav'
+            })
+            .state('notFound', {
+                url: '/notFound',
+                templateUrl: '/ngApp/views/notFound.html',
+                parent: 'nav'
+            });
+
+        // Handle request for non-existent route
+        $urlRouterProvider.otherwise('/notFound');
+
+        // Enable HTML5 navigation
+        $locationProvider.html5Mode(true);
+    });
+
+    
+
+}
---
+++ ngApp/controllers/controllers.ts
@@ -0,0 +1,33 @@
+namespace codingskills.Controllers {
+    export class NavController {
+        public role = "Provide Navigation to application"
+    }
+    export class HomeController {
+        public message = 'Hello from the home page!';
+    }
+    export class GymController {
+
+    }
+    export class CourtsideController {
+
+    }
+    export class LockerroomController {
+
+    }
+    export class ScoreboardController {
+        
+    }
+    export class AccountController {
+
+    }
+    export class MyAccountController {
+
+    }
+    export class LoginRegisterController {
+        
+    }
+    export class AboutController {
+        public message = 'Hello from the about page!';
+    }
+
+}
