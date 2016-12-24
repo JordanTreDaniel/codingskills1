6a0da7ac2d022f2396f83531faf63a500cc60689
Jordan Christley <lilj390150@live.com>
2016-12-24 00:05:02 -0600

    api calls, word is selected, word displayed, then typed + remainder of word is displayed together

--- ngApp/controllers/controllers.ts
+++ ngApp/controllers/controllers.ts
@@ -9,6 +9,28 @@ namespace codingskills.Controllers {
 
     }
     export class CourtsideController {
+        public words = [];
+        public currentWord;
+        public typed;
+        public difference;
+        public getWords() {
+            let pattern = '[a]';
+            this.$http.get('https://wordsapiv1.p.mashape.com/words/?mashape-key=L1Q3tAzB6rmshe27MNaoQquiTyTVp1aw7icjsnz3QOFVipm7Bv&letterPattern='
+                        + pattern)
+                .then((results) => {
+                    this.words = results.data['results'].data;
+                    this.currentWord = this.words[0];        
+                    this.difference = this.currentWord;
+                }).catch((err) => {
+                    console.log("Error getting the words", err);
+                });
+        }
+        public changeDifference() {
+            this.difference = this.currentWord.substring(this.typed.length);
+        }
+        constructor(private $http: ng.IHttpService) {
+            this.getWords();
+        }
 
     }
     export class LockerroomController {
