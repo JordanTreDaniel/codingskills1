ada8199fb93c3686c08e437d40ed0d8bd1571034
Jordan Christley <lilj390150@live.com>
2016-12-27 00:02:13 -0600

    The stats are tracked, the words are filtered,  and the test is completeable

--- ngApp/controllers/controllers.ts
+++ ngApp/controllers/controllers.ts
@@ -9,17 +9,28 @@ namespace codingskills.Controllers {
 
     }
     export class CourtsideController {
+        constructor(private $http: ng.IHttpService) {
+            this.getWords();
+        }
         public words = [];
         public currentWord;
         public typed;
         public difference;
+        public i = 0;
+        public statsObject = {
+            mistakes: 0,
+            wordsTyped: 0,
+            keysTyped: 0,
+            errorRate: 0
+        }
         public getWords() {
-            let pattern = '[a]';
+            let pattern = '[s]';
             this.$http.get('https://wordsapiv1.p.mashape.com/words/?mashape-key=L1Q3tAzB6rmshe27MNaoQquiTyTVp1aw7icjsnz3QOFVipm7Bv&letterPattern='
                         + pattern)
                 .then((results) => {
                     this.words = results.data['results'].data;
-                    this.currentWord = this.words[0];        
+                    console.log(this.words);
+                    this.selectWord();
                     this.difference = this.currentWord;
                 }).catch((err) => {
                     console.log("Error getting the words", err);
@@ -28,10 +39,48 @@ namespace codingskills.Controllers {
         public changeDifference() {
             this.difference = this.currentWord.substring(this.typed.length);
         }
-        constructor(private $http: ng.IHttpService) {
-            this.getWords();
-        }
+        public selectWord() {
+            this.i++;
+            let unwantedChars = new RegExp("['/ .-0-9]", 'g');
+            // let unwantedChars = new RegExp("['/]", 'g');
 
+            if (this.i >= this.words.length) {
+                console.log(this.statsObject);
+            }
+            if (this.words[this.i].match(unwantedChars)) {
+                console.log("This one has unwanted chars", this.words[this.i]);
+                this.selectWord(); //My first use of recursion!!!!
+            } else {
+                this.currentWord = this.words[this.i];
+            }
+        }
+        public keyDown(e) {
+            let word = this.currentWord,
+                typed = this.typed;
+            //update the user feedback model
+            this.changeDifference();
+            //Check for mistakes
+            if (e.keyCode == 8) {
+                console.log("BACKSPACE!");
+            } else {
+                //count the keysTyped
+                this.statsObject.keysTyped++;
+                //basically if they made a mistake
+                if (word[typed.length - 1] != typed[typed.length - 1]) {
+                    this.statsObject.mistakes++;
+                    console.log("mistakes: ", this.statsObject.mistakes); 
+                }
+            }
+            //Check for correct completion
+            if (this.typed === this.currentWord) {
+                //count the wordsTyped
+                this.statsObject.wordsTyped++;
+                //load a new word
+                this.selectWord();
+                //empty the typing input
+                this.typed = '';
+            }
+        }
     }
     export class LockerroomController {
 
