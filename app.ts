49045c09ba5f52904fc08e516032001391330ddd
j3b1d1ho0 <j3b1d1ho0@gmail.com>
2017-01-05 15:46:37 -0800

    Users branch

--- app.ts
+++ app.ts
@@ -5,26 +5,96 @@ import * as logger from 'morgan';
 import * as cookieParser from 'cookie-parser';
 import * as bodyParser from 'body-parser';
 import * as ejs from 'ejs';
+import * as express from 'express';
+import * as path from 'path';
+import * as favicon from 'serve-favicon';
+import * as logger from 'morgan';
+import * as bodyParser from 'body-parser';
+import * as ejs from 'ejs';
+import * as mongoose from 'mongoose';
+import * as passport from 'passport';
+import * as session from 'express-session';
+const MongoStore = require('connect-mongo')(session);
+import routes from './routes/index';
+import User from './models/User';
+
 
 import routes from './routes/index';
 import users from './routes/users';
 
 let app = express();
 
+//load your env vars
+if (app.get('env') === 'development') {
+  let dotenv = require('dotenv');
+  dotenv.load();
+}
+
+//config for passport login
+require("./config/passport");
+
+//config req.session your session
+app.set('trust proxy', 1); // trust first proxy
+let sess = {
+  maxAge: 172800000, // 2 days
+  secure: false,
+  httpOnly: true
+}
+
+//set to secure in production
+if (app.get('env') === 'production') {
+  sess.secure = true // serve secure cookies
+}
+
+//use session config
+app.use(session({
+  cookie: sess,
+  secret: process.env.SESSION_SECRET, // can support an array
+  store: new MongoStore({
+    url: process.env.MONGO_URI
+  }),
+  unset: 'destroy',
+  resave: false,
+  saveUninitialized: false //if nothing has changed.. do not restore cookie
+}));
+
+//connect to DB
+let dbc = mongoose.connect(process.env.MONGO_URI);
+
+mongoose.connection.on('connected', () => {
+  User.findOne({username: 'admin'}, (err, user) => {
+    if(err) return;
+    if(user) return;
+    if(!user)
+      var admin = new User();
+      admin.email = process.env.ADMIN_EMAIL;
+      admin.username = process.env.ADMIN_USERNAME;
+      admin.setPassword(process.env.ADMIN_PASSWORD);
+      admin.roles = ['user', 'admin'];
+      admin.save();
+  });
+});
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(passport.session());
 app.use(bodyParser.json());
+app.use(passport.initialize());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/bower_components', express.static(path.join(__dirname, 'bower_components')));
 app.use('/ngApp', express.static(path.join(__dirname, 'ngApp')));
 app.use('/api', express.static(path.join(__dirname, 'api')));
+
+app.use('/api', require('./api/users'));
 
 app.use('/', routes);
 app.use('/users', users);
