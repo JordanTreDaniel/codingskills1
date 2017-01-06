7db284d52b02ee30632b93b32b467fdd381aa17d
j3b1d1ho0 <j3b1d1ho0@gmail.com>
2017-01-05 17:55:57 -0800

    Lost some files, trying to recoup them. Added to controllers.

--- ngApp/controllers/controllers.ts
+++ ngApp/controllers/controllers.ts
@@ -1,9 +1,36 @@
 namespace codingskills.Controllers {
     export class NavController {
         public role = "Provide Navigation to application"
+        public currentUser;
+  public self = this;
+
+  constructor(
+    private UserService: codingSkills.Services.UserService,
+    private $state: ng.ui.IStateService,
+    currentUser: ng.ui.IResolvedState
+  ) {
+    this.currentUser = currentUser;
+  }
+
+  logout() {
+    this.UserService.logout().then(() => {
+      this.$state.go('main.home', null, {reload: true, notify:true});
+    }).catch(() => {
+      throw new Error('Unsuccessful logout');
+    });
+  }
     }
     export class HomeController {
         public message = 'Hello from the home page!';
+         public currentUser;
+  constructor(
+    private $state: ng.ui.IStateService,
+    currentUser: ng.ui.IResolvedState,
+    private $cookies: ng.cookies.ICookiesService
+  ) {
+
+    this.currentUser = currentUser;
+  }
     }
     export class GymController {
 
@@ -107,9 +134,60 @@ namespace codingskills.Controllers {
 
     }
     export class MyAccountController {
+        public avatar:string;
+        public currentUser;
+  
+  constructor(
+    currentUser: ng.ui.IResolvedState,
+    $state: ng.ui.IStateService
+  ) 
+  
+  {
+    this.currentUser = currentUser;
+    //u must b auth br0 *redirected w/ angular*
+    //should be done from stateProvider
+    if(!currentUser['username']) {
+      $state.go('main.login', null, { reload: true, notify: true });
+    }
+
+    if(currentUser['facebookId']){
+      this.avatar = `//graph.facebook.com/v2.8/${currentUser['facebookId']}/picture`;
+    } else {
+      this.avatar = '//placehold.it/350x350';
+    }
+  }
+
+
+        
 
     }
     export class LoginRegisterController {
+          public user;
+  public currentUser;
+  public isLoggedIn;
+
+  public login(user) {
+    this.UserService.login(user).then((res) => {
+      this.$state.go('main.profile', null, {reload: true, notify:true});
+    }).catch((err) => {
+      alert('Bunk login, please try again.');
+    });
+  }
+
+  public register(user) {
+    this.UserService.register(user).then((res) => {
+      this.$state.go('main.login');
+    }).catch((err) => {
+      alert('Registration error: please try again.');
+    });
+  }
+
+  constructor(
+    private UserService:codingSkills.Services.UserService,
+    private $state: ng.ui.IStateService
+  ) {
+  }
+
         
     }
     export class AboutController {
